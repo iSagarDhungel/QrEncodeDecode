510ae6521dd5743ed93a42ca44e0bd9d74311286
Sagar Dhungel <hiesagar@gmail.com>
2018-11-16 06:20:27 +0545

    Final Version Complemented

--- QRcode/Form1.Designer.cs
+++ QRcode/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEncode = new System.Windows.Forms.TextBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.camStart = new System.Windows.Forms.Button();
+            this.capture = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(288, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(325, 244);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Encode";
+            // 
+            // txtEncode
+            // 
+            this.txtEncode.Location = new System.Drawing.Point(58, 195);
+            this.txtEncode.Multiline = true;
+            this.txtEncode.Name = "txtEncode";
+            this.txtEncode.Size = new System.Drawing.Size(224, 40);
+            this.txtEncode.TabIndex = 2;
+            this.txtEncode.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(288, 262);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(66, 23);
+            this.btnEncode.TabIndex = 3;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(360, 262);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(66, 23);
+            this.btnDecode.TabIndex = 6;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Location = new System.Drawing.Point(58, 243);
+            this.txtDecode.Multiline = true;
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(224, 42);
+            this.txtDecode.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Decode";
+            // 
+            // camStart
+            // 
+            this.camStart.Location = new System.Drawing.Point(457, 262);
+            this.camStart.Name = "camStart";
+            this.camStart.Size = new System.Drawing.Size(75, 23);
+            this.camStart.TabIndex = 7;
+            this.camStart.Text = "StartCamera";
+            this.camStart.UseVisualStyleBackColor = true;
+            this.camStart.Click += new System.EventHandler(this.camStart_Click);
+            // 
+            // capture
+            // 
+            this.capture.Location = new System.Drawing.Point(538, 262);
+            this.capture.Name = "capture";
+            this.capture.Size = new System.Drawing.Size(75, 23);
+            this.capture.TabIndex = 8;
+            this.capture.Text = "Capture";
+            this.capture.UseVisualStyleBackColor = true;
+            this.capture.Click += new System.EventHandler(this.capture_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(58, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(224, 164);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(659, 311);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.capture);
+            this.Controls.Add(this.camStart);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.txtDecode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.txtEncode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEncode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.TextBox txtDecode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button camStart;
+        private System.Windows.Forms.Button capture;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- QRcode/Form1.cs
+++ QRcode/Form1.cs
@@ -1,20 +1,118 @@
-﻿using System;
+﻿using MessagingToolkit.QRCode.Codec.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
 namespace QRcode
 {
     public partial class Form1 : Form
     {
+        
+
+        //Frame
+        Image<Bgr, byte> current;
+        //Webcam
+        VideoCapture webcam = new VideoCapture();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            using(SaveFileDialog sfd = new SaveFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true })
+            {
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    MessagingToolkit.QRCode.Codec.QRCodeEncoder encoder = new MessagingToolkit.QRCode.Codec.QRCodeEncoder();
+                    encoder.QRCodeScale = 8;
+                    Bitmap bmp = encoder.Encode(txtEncode.Text);
+                    pictureBox2.Image = bmp;
+                    bmp.Save(sfd.FileName, ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames=true, Multiselect=false })
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox2.Image = Image.FromFile(ofd.FileName);
+                    MessagingToolkit.QRCode.Codec.QRCodeDecoder decoder = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+    
+                    try
+                    {
+                        txtDecode.Text = decoder.Decode(new QRCodeBitmapImage(pictureBox2.Image as Bitmap));
+                    }
+                    catch (Exception ex)
+                    {
+                        txtDecode.Text = "Text Pattern not found. NOT A QR CODE (?)";
+                    }
+                    
+                }
+            }
+        }
+        private void processCamera(object sender, EventArgs e)
+        {
+            current = webcam.QuerySmallFrame().ToImage<Bgr, Byte>();
+
+            // Flip because forecam is MIRRORED
+            //current = current.Flip(Emgu.CV.CvEnum.FLIP.HORIZONTAL);
+            pictureBox1.Image = current.Bitmap;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void camStart_Click(object sender, EventArgs e)
+        {
+            //run until close
+            Application.Idle += new EventHandler(processCamera);
+        }
+
+        private void capture_Click(object sender, EventArgs e)
+        {
+            //Get the current frame
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = pictureBox1.Image;
+
+            MessagingToolkit.QRCode.Codec.QRCodeDecoder decoder = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+            try
+            {
+                txtDecode.Text = decoder.Decode(new QRCodeBitmapImage(pictureBox2.Image as Bitmap));
+            }
+            catch (Exception ex)
+            {
+                txtDecode.Text = "Text Pattern not found. NOT A QR CODE (?)";
+            }
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
